580f63a676f1de1a9e1be5e92d767bd39985cd8a
Prithvi Bhori <imprithvi2003@gmail.com>
2025-02-15 23:54:17 +0530

    Add files via upload

---
+++ src/data/projects.js
@@ -0,0 +1,21 @@
+const projects = [
+    {
+        image: 'path/to/project-image1.jpg',
+        name: 'Blood bank management system',
+        description: 'Blood bank management which holds the reacord of donner and patients.',
+        link: {
+            github: ''
+        }
+    },
+    {
+        image: 'path/to/project-image2.jpg',
+        name: 'Number plate Detection',
+        description: 'Number plate detection using openGL.',
+        link:{
+            github: ''
+        } 
+    },
+    // Add more projects as needed
+];
+
+export default projects;
